42dcf164d131a0d3e105310db00bf02d3f17a387
aulonsal <aulonsal@gmail.com>
2021-03-30 11:52:11 +0530

    Pull secret key from config

--- steward_fastapi/core/authentication/oauth.py
+++ steward_fastapi/core/authentication/oauth.py
@@ -4,12 +4,12 @@ from typing import Optional, Union, Literal
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
+from steward_fastapi.config.config import JWT_SECRET_KEY
 from steward_fastapi.core.models.validation import Agent
 from steward_fastapi.core.models.database import Agent as AgentDB
 from passlib.context import CryptContext
 from pydantic import BaseModel
 
-SECRET_KEY = '***REMOVED***'
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 40
 
@@ -50,7 +50,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     expires = datetime.utcnow() + expires_delta if expires_delta else timedelta(minutes=15)
     to_encode.update(dict(exp=expires))
 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 async def get_current_agent(token: str = Depends(oauth2_scheme)):
@@ -60,7 +60,7 @@ async def get_current_agent(token: str = Depends(oauth2_scheme)):
                 headers={'WWW-Authenticate': 'Bearer'},
             )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get('sub')
         if username is None:
             raise credentials_exception
